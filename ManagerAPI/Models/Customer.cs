5e4da01fbe1a014e7a065e47bc45fd930632fc2d
Rado070199 <rmazur1999@gmail.com>
2023-04-12 22:22:16 +0200

    Added .gitignore and Models: customer, order and user

---
+++ ManagerAPI/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagerAPI.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+        //Relationships
+        public List<Order> Orders { get; set; }
+    }
+}
---
+++ ManagerAPI/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManagerAPI.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        //Customer
+        public int CustomerId { get; set; }
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+    }
+}
