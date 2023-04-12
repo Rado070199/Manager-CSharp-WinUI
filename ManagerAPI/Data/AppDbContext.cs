e5ce489c67ff9c510a26655b488ec840e75b78bb
Rado070199 <rmazur1999@gmail.com>
2023-04-12 22:27:44 +0200

    Added AppDbContext

---
+++ ManagerAPI/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using ManagerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Manager.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .HasMany(c => c.Orders)
+                .WithOne(o => o.Customer)
+                .HasForeignKey(o => o.CustomerId);
+        }
+    }
+}
